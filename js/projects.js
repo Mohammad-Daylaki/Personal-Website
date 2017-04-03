413f117a34c415328d64929b1c5b67917a12869d
Mohammad Daylaki <mohammad.daylaky@gmail.com>
2017-04-03 03:50:17 +0300

    Ajax-favorite

--- js/projects.js
+++ js/projects.js
@@ -4,15 +4,14 @@ $(document).ready(function() {
 })
 
 function submittedcurrent() {
-	$(".half-circle").click(function(event) {
+	$(".circle").click(function(event) {
 	event.preventDefault();
+	$(".circle").hide()
     var name=$('#name').val();
     var language=$('#language').val();
     var html_url=$('#html_url').val();
     var updated_at= $("#updated_at").val();
     var fork= $("#fork").val();
-    $('div').removeClass('half-circle');
-    $(this).addClass('half-circle2')
     getAjax(name,language,html_url,updated_at,fork);
 		event.stopPropagation();
 	})
@@ -27,18 +26,39 @@ function getAjax(name,language,html_url,updated_at,fork) {
 		type: "GET",
 		dataType: "json",
 		success: function(response){
+
 			$(".currentresult").fadeIn("slow", showResult(response));
+			var	pageClose = $('<img>').attr('src','http://www.vw-automotores.com.mx/img/exit.png')
+                           .attr('id', 'closed')
+                           .attr('width', '30')
+			$(".currentresult").append(pageClose)
+			paggeClosed();
 		}
 	})
 
 }
 
+function paggeClosed() {
+	$('#closed').click(function(){
+		$(".currentresult").fadeOut("slow");
+		$(".circle").fadeIn("slow");
+	})
+}
+
 function showResult(response) {
 				$.each(response, function(index,repo){
-var linkName= $('<a>').attr('href', repo.html_url).attr('id', 'reponame').text(repo.name);
-var repoLang = $('<h5>').text(repo.language).attr('id', 'language')
-var repoUpdate = $('<h5>').text(repo.updated_at).attr('id', 'updated')
+var linkName= $('<a>').attr('href', repo.html_url)
+                      .attr('id', 'reponame')
+                      .text(repo.name)
+                      .attr('target', '_blank');
+var repoLang = $('<h5>').text(repo.language)
+                        .attr('id', 'language')
+var repoUpdate = $('<h5>').text(repo.updated_at)
+                          .attr('id', 'updated')
 	$(".currentresult").append(linkName ,repoLang ,repoUpdate);
 	// $(".currentresult").append('<br>');
 			})
 }
+
+// -------------------------------------------------------------------------
+
